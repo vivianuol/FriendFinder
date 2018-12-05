d76a5c0815d119ae0199808442b7348e365a24fe
Si,Peng <sipeng_vivian@hotmail.com>
2018-12-04 22:16:49 -0600

    update env port to server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var api = require("./app/routing/apiRoutes")
 
 
 var app = express();
-var PORT = 8080;
+var PORT = process.env.port || 80;
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
